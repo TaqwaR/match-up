e2954de61a4adb95acacb882553c590879b962f5
taqwa <trushdan@gmail.com>
2018-03-02 23:12:46 -0500

    undefined property err

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,19 +10,29 @@ module.exports = function(app) {
 
   app.post("/api/matches", function(request, response) {
 
-    let chosenMatch {
-      name: "",
-      phots: "",
-      scoreDifference: "",
-    };
-
     console.log(request.body);
 
     let newMatch = request.body;
     let newMatchScores = newMatch.scores;
 
+    console.log(newMatch);
     console.log(newMatchScores);
 
+    const subtract = (a, b) => a - b;
+    const add = (a, b) => a + b;
 
-  });
-}
+    let newScores = newMatch.scores;
+    let totalNew = newScores.reduce(add);
+    console.log(totalNew);
+
+    for (var i = 0; i < matches.allMatchesData[i].scores.length; i++) {
+      let oldScores = matches.allMatchesData[i].scores;
+      let totalOld = oldScores.reduce(add);
+      console.log(totalOld);
+      let matchDiff = "";
+    };
+
+      //matches.allMatchesData.push(newMatch);
+
+    })
+  };
